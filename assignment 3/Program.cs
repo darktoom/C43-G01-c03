38e45cf7af67750424bbedaf1750255ac5a5cc08
darktoom <darktoom789@gmail.com>
2025-02-03 21:31:30 +0200

    Add project files.

---
+++ assignment 3/Program.cs
@@ -0,0 +1,188 @@
+﻿using System.Numerics;
+
+namespace assignment_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1-
+            //int x=Convert.ToInt32 (Console.ReadLine());
+            //  if (x % 3 == 0 && x% 4 ==0) {
+            //      Console.WriteLine("yes");
+            //  }
+            //  else
+            //      Console.WriteLine("no");
+
+            //2-
+
+            //int n = int.Parse (Console.ReadLine()!);
+
+            // if (n > 0)
+            //     Console.WriteLine("postive");
+            // else if (n < 0)
+            //     Console.WriteLine("negative");
+            // else Console.WriteLine("zero");
+
+
+            //3-
+            // int x, y, z;
+            // x = Convert.ToInt32(Console.ReadLine());
+            // y = Convert.ToInt32(Console.ReadLine());
+            // z = Convert.ToInt32(Console.ReadLine());
+            // int max = x, min = x;
+
+            //  if (y > max)
+            // { max = y; }
+            // else if (z > max)
+            // { max = z; }
+
+            //if (y < min)
+            // { min = y; }
+            // else if (z < min)
+            // { min = z; }
+
+            // Console.WriteLine( "max=" + max);
+            // Console.WriteLine("min=" + min);
+
+
+            //4-
+            //int x=Convert.ToInt32 (Console.ReadLine());
+            //if (x %2==0)
+            //    Console.WriteLine("even");
+            //else
+            //    Console.WriteLine("odd");
+
+            //5-
+
+            //char l = Convert.ToChar(Console.ReadLine());
+            //if (l =='a')
+            //    Console.WriteLine("vowel");
+            //else if (l =='e')
+            //    Console.WriteLine("vowel");
+            //else if (l=='i')
+            //    Console.WriteLine("vowel");
+            //else if(l=='o')
+
+            //    Console.WriteLine("vowel");
+            //else if (l=='u')
+            //    Console.WriteLine("vowel");
+            //else
+            //    Console.WriteLine("consonant");
+
+            //6-
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //7-
+
+            //int n = Convert.ToInt32 (Console.ReadLine());
+
+            //for (int i = 0; i <= 12; i++)
+            //{
+            //    Console.WriteLine(i * n);
+            //}
+
+            //8-
+            //int n= Convert.ToInt32 (Console.ReadLine());
+            //for (int i = 1; i <= n; i++) { 
+            //if (i%2==0)
+            //        Console.WriteLine (i);
+
+            //}
+
+
+            //9-
+            //int n, pw;
+            //n = Convert.ToInt32(Console.ReadLine());
+            //pw = Convert.ToInt32(Console.ReadLine());
+            //int Result = 1;
+            //for (int i = 1; i <= pw; i++)
+
+            //{
+            //Result *= n;
+            //}
+            //Console.WriteLine(Result);
+
+            //10-
+            //int sub1, sub2, sub3, sub4, sub5;
+            //sub1=Convert.ToInt32(Console.ReadLine());
+            //sub2=Convert.ToInt32(Console.ReadLine());
+            //sub3=Convert.ToInt32(Console.ReadLine());
+            //sub4=Convert.ToInt32(Console.ReadLine());
+            //sub5=Convert.ToInt32(Console.ReadLine());
+            //int sum = sub1 + sub2 + sub3 + sub4 + sub5;
+            //int avg = sum / 5;
+            //double percent = (sum / 500) * 100;
+            //Console.WriteLine(sum);
+            //Console.WriteLine(avg);
+            //Console.WriteLine(percent);
+
+            //11-
+
+            //int month = Convert.ToInt32(Console.ReadLine());
+            //    switch (month) {
+            //    case 1:
+            //        Console.WriteLine("days 31");
+            //        break;
+            //        case 2:
+            //        Console.WriteLine("days 29");
+            //        break;
+            //        case 3:
+            //        Console.WriteLine("days 31");
+
+            //break;
+            //        case 4:
+            //        Console.WriteLine("days 30");
+            //            break;
+            //    case 5:
+            //        Console.WriteLine("days 31");
+            //        break;
+            //        case 6:
+            //        Console.WriteLine("days 30");
+            //        break;
+            //        case 7:
+            //        Console.WriteLine("days 31")
+            //            ; break;
+            //        case 8:
+            //        Console.WriteLine("days 31");
+            //        break;
+            //        case 9:
+            //        Console.WriteLine("days 30");
+            //        break;
+            //        case 10:
+            //        Console.WriteLine("days 31");
+            //        break;
+            //        case 11:
+            //        Console.WriteLine("days 30");
+            //        break;
+            //        case 12:
+            //        Console.WriteLine("days 31");
+            //        break;
+
+            //  }
+            //12-
+            //Console.WriteLine("pls, enter operation");
+            //char op = Convert.ToChar(Console.ReadLine());
+            //Console.WriteLine("pls enter number ");
+            //int num1, num2;
+            //num1 = Convert.ToInt32(Console.ReadLine()); 
+            //num2= Convert.ToInt32(Console.ReadLine());
+            //if (op=='+')
+            //    Console.WriteLine(num1+num2);
+            //else if (op=='-')
+            //    Console.WriteLine(num1-num2);
+            //else if (op== '*')
+            //    Console.WriteLine(num1*num2);
+            //else if (op=='/')
+            //    Console.WriteLine(num2/num1);
+            //else
+            //    Console.WriteLine("invaild op char");
+            
+
+        }
+    }
+}
